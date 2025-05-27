c30a756e0b6b4915e197e7a9b56cb6f8cb24ee4b
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-05-27 16:00:23 +0700

    push to pull

--- TerrariumGardenTech.Service/Service/TerrariumService.cs
+++ TerrariumGardenTech.Service/Service/TerrariumService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TerrariumGardenTech.Common;
+using TerrariumGardenTech.Repositories;
 using TerrariumGardenTech.Repositories.Entity;
 using TerrariumGardenTech.Service.Base;
 using TerrariumGardenTech.Service.IService;
@@ -11,37 +13,90 @@ namespace TerrariumGardenTech.Service.Service
 {
     public class TerrariumService : ITerrariumService
     {
-        public Task<IBusinessResult> CreateClub()
+
+        private readonly UnitOfWork _unitOfWork;
+        public TerrariumService(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        }
+
+        public async Task<IBusinessResult> GetAll()
+        {
+            var terrariumList = _unitOfWork.Terrarium.GetAllAsync();
+            if (terrariumList != null)
+            {
+                return new BusinessResult(Const.SUCCESS_READ_CODE,Const.SUCCESS_READ_MSG, await terrariumList);
+            }
+            else
+            {
+                return new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG);       
+            }
+
+        }
+
+        public Task<IBusinessResult> GetById(int id)
+        {
+            var terrarium = _unitOfWork.Terrarium.GetByIdAsync(id);
+            if (terrarium != null)
+            {
+                return Task.FromResult<IBusinessResult>(new BusinessResult(Const.SUCCESS_READ_CODE, Const.SUCCESS_READ_MSG, terrarium));
+            }
+            else
+            {
+                return Task.FromResult<IBusinessResult>(new BusinessResult(Const.WARNING_NO_DATA_CODE, Const.WARNING_NO_DATA_MSG));
+            }
+        }
+
+        
+        public async Task<IBusinessResult> Save(Terrarium terrarium)
+        {
+            try
+            {
+                int result = -1;
+                var terrariumEntity = _unitOfWork.Terrarium.GetById(terrarium.TerrariumId);
+                if (terrariumEntity != null)
+                {
+                    result = await _unitOfWork.Terrarium.UpdateAsync(terrarium);
+                    if (result > 0)
+                    {
+                        return new BusinessResult(Const.SUCCESS_UPDATE_CODE, Const.SUCCESS_UPDATE_MSG, terrarium);
+                    }
+                    else
+                    {
+                        return new BusinessResult(Const.FAIL_UPDATE_CODE, Const.FAIL_UPDATE_MSG);
+                    }
+                }else{
+                    // Create new terrarium if it does not exist
+                    result = await _unitOfWork.Terrarium.CreateAsync(terrarium);
+                    if (result > 0)
+                    {
+                        return new BusinessResult(Const.SUCCESS_CREATE_CODE, Const.SUCCESS_CREATE_MSG, terrarium);
+                    }
+                    else
+                    {
+                        return new BusinessResult(Const.FAIL_CREATE_CODE, Const.FAIL_CREATE_MSG);
+                    }
+                }
+                
+                
+            }catch (Exception ex)
+            {
+                return new BusinessResult(Const.ERROR_EXCEPTION, ex.ToString());
+            }
+    
+            
+        }
+
+        public Task<IBusinessResult> UpdateTerrarium()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<IBusinessResult> CreateTerrarium()
         {
             throw new NotImplementedException();
         }
 
         public Task<IBusinessResult> DeleteById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IBusinessResult> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IBusinessResult> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IBusinessResult> GetClubName()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IBusinessResult> Save(Terrarium terrarium)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IBusinessResult> UpdateClub()
         {
             throw new NotImplementedException();
         }
