58bcdcf13381da369473caa7bd8bbd8676b16663
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-05-27 15:43:53 +0700

    Login & register jwt basic

---
+++ TerrariumGardenTech.Service/IService/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Repositories.Entity;
+using TerrariumGardenTech.Service.RequestModel;
+
+namespace TerrariumGardenTech.Service.IService
+{
+    public interface IUserService
+    {
+        Task<bool> RegisterUserAsync(UserRegisterRequest userRequest);
+        Task<string> LoginAsync(string username, string password);
+    }
+}
