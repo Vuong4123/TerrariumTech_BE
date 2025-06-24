a960357989564e65d78f5ee33f7ddc09be5c2329
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-06-24 21:21:34 +0700

    Base Personalize Service

--- TerrariumGardenTech.Repositories/Entity/Personalize.cs
+++ TerrariumGardenTech.Repositories/Entity/Personalize.cs
@@ -3,22 +3,20 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TerrariumGardenTech.Repositories.Entity;
 
 public partial class Personalize : BaseEntity
 {
     [Key]
-
     public int PersonalizeId { get; set; }
-
     public int UserId { get; set; }
-
-    public string Preferences { get; set; }
-
+    public string Type { get; set; }
+    public string Shape { get; set; }
+    public string TankMethod { get; set; }
     public string Theme { get; set; }
-
     public string Language { get; set; }
-
+    [ForeignKey(nameof(UserId))]
     public virtual User User { get; set; }
 }
