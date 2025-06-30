95c0ed677a99919ca7c2319037ba058038fe793d
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-06-30 15:53:07 +0700

    Done: crud shape

---
+++ TerrariumGardenTech.Service/RequestModel/Shape/ShapeUpdateRequest.cs
@@ -0,0 +1,13 @@
+namespace TerrariumGardenTech.Service.RequestModel.Shape;
+public class ShapeUpdateRequest
+{
+    public int ShapeId { get; set; }
+    public string ShapeName { get; set; } = string.Empty;
+    public string ShapeDescription { get; set; }    = string.Empty;
+    public string ShapeSize { get; set; } = string.Empty;
+    public int ShapeHeight { get; set; }
+    public int ShapeWidth { get; set; }
+    public int ShapeLength { get; set; }
+    public float ShapeVolume { get; set; }
+    public string ShapeMaterial { get; set; } = string.Empty;
+}
