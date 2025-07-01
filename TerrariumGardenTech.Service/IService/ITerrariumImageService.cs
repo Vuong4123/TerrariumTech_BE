e10bb16ec34e81721b317928e8c76b2d091d9b59
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-01 20:41:41 +0700

    pull new code

---
+++ TerrariumGardenTech.Service/IService/ITerrariumImageService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Service.Base;
+using TerrariumGardenTech.Service.RequestModel.Environment;
+
+namespace TerrariumGardenTech.Service.IService
+{
+    public interface ITerrariumImageService
+    {
+        Task<IBusinessResult> CreateTerrariumImageAsync(EnvironmentCreateRequest environmentCreateRequest);
+        Task<IBusinessResult> UpdateTerrariumImageAsync(EnvironmentUpdateRequest environmentUpdateRequest);
+        Task<IBusinessResult> DeleteTerrariumImageAsync(int environmentId);
+        Task<IBusinessResult?> GetTerrariumImageByIdAsync(int environmentId);
+        Task<IBusinessResult> GetAllTerrariumImageAsync();
+    }
+}
