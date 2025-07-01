0c039383dbeea1a639914e43e529f67827269350
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-01 15:23:11 +0700

    done: request model of TankMethod and Environment

---
+++ TerrariumGardenTech.Service/RequestModel/Environment/EnvironmentCreateRequest.cs
@@ -0,0 +1,7 @@
+namespace TerrariumGardenTech.Service.RequestModel.Environment;
+public class EnvironmentCreateRequest
+{
+    public string EnvironmentName { get; set; } = string.Empty;
+    public string EnvironmentDescription { get; set; } = string.Empty;
+
+}
