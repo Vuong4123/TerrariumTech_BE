ad11f35a25e9e95117cfbdc1935c1258b0c61a7b
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-01 21:35:40 +0700

    done: crud terrrariumImage

--- TerrariumGardenTech.API/Controllers/TerrariumImageController.cs
+++ TerrariumGardenTech.API/Controllers/TerrariumImageController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TerrariumGardenTech.Service.Base;
+using TerrariumGardenTech.Service.IService;
+using TerrariumGardenTech.Service.RequestModel.TerrariumImage;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,44 @@ namespace TerrariumGardenTech.API.Controllers
     [ApiController]
     public class TerrariumImageController : ControllerBase
     {
-        // GET: api/<TerrariumImageController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly ITerrariumImageService _terrariumImageService;
+        public TerrariumImageController(ITerrariumImageService terrariumImageService)
         {
-            return new string[] { "value1", "value2" };
+            _terrariumImageService = terrariumImageService;
+        }
+        // GET: api/<TerrariumImageController>
+        [HttpGet("get-all")]
+        public async Task<IBusinessResult> Get()
+        {
+            return await _terrariumImageService.GetAllTerrariumImageAsync();
         }
 
         // GET api/<TerrariumImageController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("get-{id}")]
+        public async Task<IBusinessResult?> Get(int id)
         {
-            return "value";
+            return await _terrariumImageService.GetTerrariumImageByIdAsync(id);
         }
 
         // POST api/<TerrariumImageController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        [HttpPost("add-terrariumImage")]
+        public async Task<IBusinessResult> Post([FromBody] TerrariumImageCreateRequest terrariumImageCreateRequest)
         {
+            return await _terrariumImageService.CreateTerrariumImageAsync(terrariumImageCreateRequest); 
         }
 
         // PUT api/<TerrariumImageController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [HttpPut("update-terrariumImage-{id}")]
+        public async Task<IBusinessResult> Put([FromBody] TerrariumImageUpdateRequest terrariumImageUpdateRequest)
         {
+            return await _terrariumImageService.UpdateTerrariumImageAsync(terrariumImageUpdateRequest); 
         }
 
         // DELETE api/<TerrariumImageController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
+        [HttpDelete("delete-terrariumImage-{id}")]
+        public async Task<IBusinessResult> Delete(int id)
         {
+            return await _terrariumImageService.DeleteTerrariumImageAsync(id); 
         }
     }
 }
