b90c8d4f2ee533cb36984422f77bca5cda9f4940
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-03 17:37:19 +0700

    validate voucher
    validate voucher

---
+++ TerrariumGardenTech.Service/ResponseModel/Voucher/VoucherResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Service.ResponseModel.Voucher
+{
+    public class VoucherResponse
+    {
+        public int VoucherId { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public DateTime ValidFrom { get; set; }  // Ensure this is DateTime
+        public DateTime ValidTo { get; set; }    // Ensure this is DateTime
+        public string Status { get; set; }
+    }
+
+
+}
