5c7721bd3ce89ebd096b0cf9d5a322865822a600
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-03 20:37:30 +0700

    update-API-Account
    update-API-Account

---
+++ TerrariumGardenTech.Repositories/Enums/RoleStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Repositories.Enums
+{
+    public enum RoleStatus
+    {
+        User = 1,    // RoleId = 1
+        Staff = 2,   // RoleId = 2
+        Manager = 3, // RoleId = 3
+        Admin = 4,   // RoleId = 4
+        Shipper = 5  // RoleId = 5
+    }
+}
