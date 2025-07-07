46588f47f93f46e190ef68b5de6b938306072afb
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-07 19:45:03 +0700

    fearture code: delete Shape and TankMethod

--- TerrariumGardenTech.Repositories/Repositories/TerrariumTankMethodRepository.cs
+++ TerrariumGardenTech.Repositories/Repositories/TerrariumTankMethodRepository.cs
@@ -11,6 +11,16 @@ public class TerrariumTankMethodRepository : GenericRepository<TerrariumTankMeth
     //public TerrariumTankMethodRepository() { }
 
     public TerrariumTankMethodRepository(TerrariumGardenTechDBContext dbContext) => _dbContext = dbContext;
+
+    public async Task<List<TerrariumTankMethod>> GetAllTerrariumByTankMethods(int tankMethodId)
+        {
+            return await _dbContext
+                .TerrariumTankMethods
+                .AsNoTracking()
+                .Include(te => te.Terrarium)
+                .Where(te => te.TankMethodId == tankMethodId)
+                .ToListAsync();
+        }
     public async Task<List<TerrariumTankMethod>> GetTankMethodsByTerrariumId(int terrariumId)
     {
         return await _dbContext.TerrariumTankMethods
