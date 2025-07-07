a52f3f62101ffab12eaf9414681c74c4c9a6b20d
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-07 13:44:46 +0700

    feat: coding add userContext in system

--- TerrariumGardenTech.Service/IService/IMembershipService.cs
+++ TerrariumGardenTech.Service/IService/IMembershipService.cs
@@ -4,7 +4,7 @@ namespace TerrariumGardenTech.Service.IService
 {
     public interface IMembershipService
     {
-        Task<int> CreateMembershipAsync(int userId, string membershipType, DateTime startDate, DateTime endDate, string status);
+        Task<int> CreateMembershipAsync(string membershipType, DateTime startDate, DateTime endDate, string status);
         Task<List<Membership>> GetAllMembershipsAsync();
         Task<Membership> GetMembershipByIdAsync(int membershipId);
         Task<List<Membership>> GetMembershipsByUserIdAsync(int userId);
