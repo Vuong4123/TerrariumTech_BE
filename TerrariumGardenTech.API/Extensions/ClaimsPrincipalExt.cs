89f34ea1d3c08caec2754fb177cb81b0d7a0e69e
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-08 14:48:32 +0700

    API Order
    Order

---
+++ TerrariumGardenTech.API/Extensions/ClaimsPrincipalExt.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace TerrariumGardenTech.API.Extensions
+{
+    public static class ClaimsPrincipalExt
+    {
+        public static int GetUserId(this ClaimsPrincipal u) =>
+            int.Parse(u.FindFirstValue(ClaimTypes.NameIdentifier)!);
+    }
+}
