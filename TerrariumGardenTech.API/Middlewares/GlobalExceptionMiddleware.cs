94cc3d608c7e48f7e974a0cc065fa6a99b613720
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-15 17:06:05 +0700

    fix membership

---
+++ TerrariumGardenTech.API/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using TerrariumGardenTech.Common;
+using TerrariumGardenTech.Service.Base;
+using System.Text.Json;
+
+namespace TerrariumGardenTech.API.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                await HandleExceptionAsync(context, Const.UNAUTHORIZED_CODE, Const.UNAUTHORIZED_MSG, ex);
+            }
+            catch (ArgumentException ex)
+            {
+                await HandleExceptionAsync(context, Const.FAIL_CREATE_CODE, ex.Message, ex);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, Const.ERROR_EXCEPTION, "Lỗi hệ thống", ex);
+            }
+        }
+
+        private async Task HandleExceptionAsync(HttpContext context, int statusCode, string message, Exception ex)
+        {
+            _logger.LogError(ex, "Lỗi toàn cục");
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+
+            var result = new BusinessResult
+            {
+                Status = statusCode,
+                Message = message,
+                Data = null
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(result));
+        }
+    }
+}
