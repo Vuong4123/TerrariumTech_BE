754206bfa219ac39736e6463b0a6d08a4ec5c781
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-17 00:16:32 +0700

    Update checkout for Order

---
+++ TerrariumGardenTech.Repositories/Repositories/PaymentTransitionRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Repositories.Base;
+using TerrariumGardenTech.Repositories.Entity;
+
+namespace TerrariumGardenTech.Repositories.Repositories
+{
+    public class PaymentTransitionRepository : GenericRepository<PaymentTransition>
+    {
+        public PaymentTransitionRepository(TerrariumGardenTechDBContext context) : base(context) { }
+
+        public async Task AddAsync(PaymentTransition entity)
+        {
+            await _context.Set<PaymentTransition>().AddAsync(entity);
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+
+}
