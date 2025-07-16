fec1df42edb5056cbe1e067b701208f4c25188be
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-16 20:48:56 +0700

    fix db

---
+++ TerrariumGardenTech.Repositories/Entity/MembershipPackage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Repositories.Entity
+{
+    public class MembershipPackage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; } = string.Empty; // VD: "1Month", "3Months"
+
+        [Required]
+        public int DurationDays { get; set; } // 30, 90, 365...
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; } // Giá hiện tại của gói
+
+        public string? Description { get; set; } // Tuỳ chọn: mô tả thêm
+
+        public bool IsActive { get; set; } = true; // Có cho phép mua không?
+
+        public ICollection<Membership> Memberships { get; set; } // Navigation
+    }
+}
