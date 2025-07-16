676997d994cd270f2d49e658a510e8669e644916
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-16 23:09:01 +0700

    fix db terrrarium

---
+++ TerrariumGardenTech.Repositories/Migrations/20250716160812_DB167V3.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TerrariumGardenTech.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class DB167V3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Terrariums",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Terrariums",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
