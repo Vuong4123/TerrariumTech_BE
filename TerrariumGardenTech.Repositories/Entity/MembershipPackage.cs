84c2a98e6bb435f01dc039a8ed2bd5ead044a575
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-16 20:25:58 +0700

    push to pull

---
+++ TerrariumGardenTech.Repositories/Entity/MembershipPackage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Repositories.Entity
+{
+    public class MembershipPackage
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; } = string.Empty; // VD: "1Month", "3Months"
+
+        [Required]
+        public int DurationDays { get; set; } // 30, 90, 365...
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public decimal Price { get; set; } // 49_000m
+
+        public string? Description { get; set; } // Tùy chọn: mô tả cho admin/frontend
+
+        public bool IsActive { get; set; } = true; // Cho phép tắt tạm thời
+
+        public ICollection<Membership> Memberships { get; set; }
+    }
+
+}
---
+++ TerrariumGardenTech.Service/RequestModel/MemberShip/CreateMembershipForUserRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Service.RequestModel.MemberShip
+{
+    public class CreateMembershipForUserRequest
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public string MembershipType { get; set; } = string.Empty;
+
+        [Required]
+        public DateTime StartDate { get; set; }
+    }
+
+}
