978da51baf95cb422a67eb0214357c762807ac23
Vuong Chan <tranminhvuong2k3@gmail.com>
2025-07-18 16:42:46 +0700

    Add automapper with api payment

---
+++ TerrariumGardenTech.Service/Service/PayOsService.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Net.payOS;
+using Net.payOS.Types;
+using TerrariumGardenTech.Common;
+using TerrariumGardenTech.Repositories;
+using TerrariumGardenTech.Service.Base;
+using TerrariumGardenTech.Service.IService;
+using TerrariumGardenTech.Service.RequestModel.Payment;
+using TerrariumGardenTech.Service.ResponseModel.Order;
+
+namespace TerrariumGardenTech.Service.Service;
+
+public class PayOsService : IPayOsService
+{
+    private readonly PayOS _payOS;
+    private readonly UnitOfWork _unitOfWork;
+    private readonly IConfiguration _config;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    protected readonly IMapper _mapper;
+
+    public PayOsService(IHttpContextAccessor httpContextAccessor,
+        UnitOfWork unitOfWork, IConfiguration config, IMapper mapper)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+        _payOS = new PayOS(
+            config["PayOS:ClientId"],
+            config["PayOS:ApiKey"],
+            config["PayOS:ChecksumKey"]
+        );
+        _config = config;
+        _mapper = mapper;
+    }
+
+    public async Task<IBusinessResult> CreatePaymentLink(int orderId, string description)
+    {
+        var order = await _unitOfWork.OrderRepository.GetByIdWithOrderItemsAsync(orderId);
+        if (order == null) return new BusinessResult(Const.NOT_FOUND_CODE, "No data found.");
+
+        var itemDatas = new List<ItemData>();
+        var amount = 0;
+        foreach (var orderItem in order.OrderItems)
+        {
+            itemDatas.Add(new ItemData(orderItem.TerrariumVariant.VariantName, orderItem.Quantity ?? 0,
+                (int)(orderItem.UnitPrice ?? 0)));
+            amount += (int)(orderItem.TotalPrice ?? 0);
+        }
+
+        var domain =
+            $"{_httpContextAccessor.HttpContext?.Request.Scheme}://{_httpContextAccessor.HttpContext?.Request.Host}/api";
+        var successReturnUrl = $"{domain}/payment?orderId={orderId}&status={PaymentReturnModelStatus.Success}";
+        var failReturnUrl = $"{domain}/payment?orderId={orderId}&status={PaymentReturnModelStatus.Fail}";
+        var orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
+
+        var signatureData =
+            $"amount={order.TotalAmount}&cancelUrl={failReturnUrl}&description={description}&orderCode={orderCode}&returnUrl={successReturnUrl}";
+        var signature = CreateHmacSha256(signatureData, _config["PayOS:ChecksumKey"]);
+
+        var paymentLinkRequest = new PaymentData(
+            orderCode: orderCode,
+            amount: amount,
+            description,
+            items: itemDatas,
+            returnUrl: successReturnUrl,
+            cancelUrl: failReturnUrl,
+            expiredAt: (int)(DateTimeOffset.UtcNow.AddMinutes(10).ToUnixTimeSeconds()),
+            signature: signature
+        );
+        var response = await _payOS.createPaymentLink(paymentLinkRequest);
+        return new BusinessResult(Const.SUCCESS_CREATE_CODE, "Success", response.checkoutUrl);
+    }
+
+    public async Task<IBusinessResult> ProcessPaymentCallback(PaymentReturnModel returnModel)
+    {
+        try
+        {
+            var order = await _unitOfWork.OrderRepository.GetByIdAsync(returnModel.OrderId);
+            if (order == null) return new BusinessResult(Const.NOT_FOUND_CODE, "No data found.");
+
+            order.PaymentStatus = "Paid";
+            await _unitOfWork.OrderRepository.UpdateAsync(order);
+            await _unitOfWork.SaveAsync();
+            var orderResponse = _mapper.Map<OrderResponse>(order);
+            return new BusinessResult(Const.SUCCESS_UPDATE_CODE, Const.SUCCESS_UPDATE_MSG, orderResponse);
+        }
+        catch (Exception e)
+        {
+            return new BusinessResult(Const.ERROR_EXCEPTION, e.Message);
+        }
+    }
+
+    private static string CreateHmacSha256(string data, string? key)
+    {
+        using (var hmac = new System.Security.Cryptography.HMACSHA256(System.Text.Encoding.UTF8.GetBytes(key)))
+        {
+            var hash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(hash).Replace("-", "").ToLower();
+        }
+    }
+}
