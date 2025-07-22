a83b5028c9688977c7724bfbf4825c4c3eea7097
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-07-22 16:19:55 +0700

    chỉnh lại logic Variant và Terrarium

--- TerrariumGardenTech.Service/RequestModel/TerrariumVariant/TerrariumVariantUpdateRequest.cs.cs
+++ TerrariumGardenTech.Service/RequestModel/TerrariumVariant/TerrariumVariantUpdateRequest.cs.cs
@@ -1,4 +1,7 @@
-﻿namespace TerrariumGardenTech.Service.RequestModel.TerrariumVariant;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TerrariumGardenTech.Service.RequestModel.TerrariumVariant;
 
 public class TerrariumVariantUpdateRequest
 {
@@ -8,7 +11,10 @@ public class TerrariumVariantUpdateRequest
 
     public string VariantName { get; set; } = string.Empty;
 
-    public decimal? Price { get; set; }
+    public decimal Price { get; set; }
+    [FromForm]
+    public IFormFile? ImageFile { get; set; } // Thay UrlImage bằng file upload
+    public int StockQuantity { get; set; }
 
-    public int? StockQuantity { get; set; }
+    public DateTime? UpdatedAt { get; set; }
 }
