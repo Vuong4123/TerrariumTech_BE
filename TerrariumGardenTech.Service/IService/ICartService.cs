e9050cced933f788b9cfa79b22a629b934a6f1d8
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-25 03:32:56 +0700

    fix cart

--- TerrariumGardenTech.Service/IService/ICartService.cs
+++ TerrariumGardenTech.Service/IService/ICartService.cs
@@ -1,6 +1,7 @@
 ﻿using TerrariumGardenTech.Common.Entity;
 using TerrariumGardenTech.Common.RequestModel.Cart;
 using TerrariumGardenTech.Common.RequestModel.Order;
+using TerrariumGardenTech.Common.ResponseModel.Cart;
 using TerrariumGardenTech.Repositories.Entity;
 
 namespace TerrariumGardenTech.Service.IService;
@@ -9,8 +10,11 @@ public interface ICartService
 {
     Task<Cart> GetOrCreateCartAsync(int userId);
     Task<Cart> GetCartByUserAsync(int userId);
-    Task<CartItem> AddItemAsync(int userId, AddCartItemRequest req);
-    Task<bool> UpdateItemAsync(int userId, int itemId, int quantity);
+    Task<CartItemResponse> AddItemAsync(int userId, AddCartItemRequest req);
+
+    // Cập nhật phương thức UpdateItemAsync để hỗ trợ 4 tham số
+    Task<bool> UpdateItemAsync(int userId, int itemId, int? accessoryQuantity, int? variantQuantity);
+
     Task<bool> RemoveItemAsync(int userId, int itemId);
     Task<bool> ClearCartAsync(int userId);
     Task<Order> CheckoutAsync(int userId, CheckoutRequest req);
