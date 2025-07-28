1de8f9374c84f9d316f06c648cdfbc10a39be782
Vuong Chan <tranminhvuong2k3@gmail.com>
2025-07-28 10:47:07 +0700

    Update

---
+++ TerrariumGardenTech.Repositories/Migrations/20250722092953_Update.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TerrariumGardenTech.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "additionalPrice",
+                table: "TerrariumVariant",
+                type: "decimal(12,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,2)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UrlImage",
+                table: "TerrariumVariant",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MinPrice",
+                table: "Terrariums",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MaxPrice",
+                table: "Terrariums",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlImage",
+                table: "TerrariumVariant");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "additionalPrice",
+                table: "TerrariumVariant",
+                type: "decimal(12,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(12,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MinPrice",
+                table: "Terrariums",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MaxPrice",
+                table: "Terrariums",
+                type: "decimal(18,2)",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
