43a01ef21e265576bd75bd5ce6550c1fd3cc8ac5
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-28 14:49:59 +0700

    Chat AI

---
+++ TerrariumGardenTech.Service/Service/ChatBotService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Repositories.Repositories;
+using TerrariumGardenTech.Service.IService;
+
+namespace TerrariumGardenTech.Service.Service
+{
+    public class ChatBotService
+    {
+        private readonly ChatGPTRepository _chatGPTRepository;
+
+        public ChatBotService()
+        {
+            _chatGPTRepository = new ChatGPTRepository();
+        }
+
+        public async Task<string> GetChatResponseAsync(string userInput)
+        {
+            return await _chatGPTRepository.GetResponseFromGPTAsync(userInput);
+        }
+    }
+}
