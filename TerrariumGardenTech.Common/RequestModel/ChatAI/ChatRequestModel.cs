c002bef74f1fae7440338b863e1c6546d00e235b
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-28 14:49:59 +0700

    Chat AI

---
+++ TerrariumGardenTech.Common/RequestModel/ChatAI/ChatRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariumGardenTech.Common.RequestModel.ChatAI
+{
+    public class ChatRequestModel
+    {
+        public string UserInput { get; set; }
+    }
+}
