6cfa7a3911724875dfdabe8cbd86c5971adbb159
Vuong Chan <tranminhvuong2k3@gmail.com>
2025-07-28 13:04:03 +0700

    Update db and CHANGE api payment (payos, vnpay)

---
+++ TerrariumGardenTech.Service/IService/IVnPayService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using TerrariumGardenTech.Common.RequestModel.Payment;
+using TerrariumGardenTech.Common.ResponseModel;
+using TerrariumGardenTech.Service.Base;
+
+namespace TerrariumGardenTech.Service.IService;
+
+public interface IVnPayService
+{
+    Task<IBusinessResult> CreatePaymentUrl(PaymentInformationModel model, HttpContext context);
+    Task<IBusinessResult> PaymentExecute(IQueryCollection collections);
+}
