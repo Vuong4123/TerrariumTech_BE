2b5462546a848f5f5293566dd835abba6f9461fa
NXQuangWIn <92784897+NXQuangWIn@users.noreply.github.com>
2025-07-29 03:22:32 +0700

    Feedback API

---
+++ TerrariumGardenTech.Service/Mappers/FeedbackProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Repositories.Entity;
+using TerrariumGardenTech.Common.RequestModel.Feedback;
+using TerrariumGardenTech.Common.ResponseModel.Feedback;
+
+namespace TerrariumGardenTech.Service.Mappers
+{
+    public class FeedbackProfile : Profile
+    {
+        public FeedbackProfile()
+        {
+            CreateMap<FeedbackCreateRequest, Feedback>()
+                .ForMember(d => d.CreatedAt, o => o.MapFrom(_ => DateTime.UtcNow));
+
+            CreateMap<Feedback, FeedbackResponse>()
+                .ForMember(d => d.Images, o => o.MapFrom(s => s.FeedbackImages.Select(i => i.ImageUrl)));
+        }
+    }
+}
---
+++ TerrariumGardenTech.Service/Service/FeedbackService.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariumGardenTech.Repositories;
+using TerrariumGardenTech.Repositories.Entity;
+using TerrariumGardenTech.Service.IService;
+using TerrariumGardenTech.Common.RequestModel.Feedback;
+using TerrariumGardenTech.Common.ResponseModel.Feedback;
+
+namespace TerrariumGardenTech.Service.Service
+{
+    public class FeedbackService : IFeedbackService
+    {
+        private readonly UnitOfWork _uow;
+        private readonly IMapper _mapper;
+
+        public FeedbackService(UnitOfWork uow, IMapper mapper)
+        {
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        public async Task<FeedbackResponse> CreateAsync(FeedbackCreateRequest request)
+        {
+            var entity = _mapper.Map<Feedback>(request);
+            await _uow.Feedback.CreateAsync(entity);
+            await _uow.SaveAsync();
+
+            return _mapper.Map<FeedbackResponse>(entity);
+        }
+
+        public async Task<List<FeedbackResponse>> GetByOrderItemAsync(int orderItemId)
+        {
+            var list = await _uow.Feedback.GetByOrderItemAsync(orderItemId);
+            return _mapper.Map<List<FeedbackResponse>>(list);
+        }
+    }
+}
