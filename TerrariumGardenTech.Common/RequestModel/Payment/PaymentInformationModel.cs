949e98a932f771fa12ee80ffce1f1cfcbccc38ad
Vuong4123 <tranminhvuong2k3@gmail.com>
2025-08-11 14:08:00 +0700

    fix payment

--- TerrariumGardenTech.Common/RequestModel/Payment/PaymentInformationModel.cs
+++ TerrariumGardenTech.Common/RequestModel/Payment/PaymentInformationModel.cs
@@ -6,7 +6,7 @@ public class PaymentInformationModel
     public string OrderType { get; set; }
     public string OrderDescription { get; set; }
     public string Name { get; set; }
-    public decimal Price { get; set; }
+    //public decimal Price { get; set; }
 }
 public class PaymentResponseModel
 {
